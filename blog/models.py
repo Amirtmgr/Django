cefc7cb1d807f13fa03047523cb0d92168e0ec98
Amir Mgr <athapa@eeposit.com>
2020-07-11 13:25:39 +0545

    Comment model added.

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    #Assigning types of properties using models
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # Assigning types of properties using models
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -20,3 +21,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.text
